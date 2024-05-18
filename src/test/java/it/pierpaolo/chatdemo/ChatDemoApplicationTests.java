791728a2301309ef819b7613d95d535af0778415
Pierpaolo D'Angelo <pierpaolodangelo96@gmail.com>
2024-05-18 14:18:52 +0200

    First commit

---
+++ src/test/java/it/pierpaolo/chatdemo/ChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pierpaolo.chatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
